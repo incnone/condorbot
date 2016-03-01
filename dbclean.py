d9d600a383e44cb0c4f205c59d092f42c979df39
incnone <incohatus.none+git@gmail.com>
2016-03-01 18:27:40 -0500

    delete code in dbclean b/c it's dangerous

--- dbclean.py
+++ dbclean.py
@@ -1,15 +1,1 @@
-import sqlite3
 
-DB_NAME = 'data/condors4.db'
-
-db_conn = sqlite3.connect(DB_NAME)
-
-try:
-    params = (3,)
-    db_conn.execute("DELETE FROM match_data WHERE week_number=?", params)
-    db_conn.commit()
-except Exception as e:
-    print('Error deleting.')
-    db_conn.rollback()
-finally:
-    db_conn.close()
