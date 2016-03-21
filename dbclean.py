c78a1e9a8670468f1592e2d9444b3f5b0d637f45
incnone <incohatus.none+git@gmail.com>
2016-03-20 22:07:59 -0400

    hotfix for bugged rooms

--- dbclean.py
+++ dbclean.py
@@ -1,1 +1,9 @@
+import sqlite3
 
+db_conn = sqlite3.connect('data/condors4.db')
+
+param_list = [(27, 10,), (1,35,), (46, 33,)]
+for params in param_list:
+    db_conn.execute("DELETE FROM channel_data WHERE racer_1_id=? AND racer_2_id=?", params)
+
+db_conn.commit()
