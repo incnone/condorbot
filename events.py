e88aba7066fcadd272b1b13fb859725007967136
Alexis Martin <alexis.yj@gmail.com>
2016-09-01 16:55:43 +0200

    Add events handling - Careful Not Tested

---
+++ events.py
@@ -0,0 +1,20 @@
+from socketIO_client import SocketIO, LoggingNamespace
+
+class Events():
+
+    def racesoon(racer_1, racer_2):
+        self._send_event('racesoon', {'racer1': racer_1, 'racer2': racer_2})
+
+    def racestart(racer_1, racer_2):
+        self._send_event('racestart', {'racer1': racer_1, 'racer2': racer_2})
+
+    def raceend(racer_1, racer_2, winner):
+        self._send_event('raceend', {'racer1': racer_1, 'racer2': racer_2, 'winner': winner})
+
+    def matchend(racer_1, racer_2):
+        self._send_event('matchend', {'racer1': racer_1, 'racer2': racer_2})
+
+    def _send_event(event, data):
+        if(EVENTS_ACTIVATED):
+            with SocketIO(EVENTS_SERVER, EVENTS_PORT, LoggingNamespace) as socketIO:
+                socketIO.emit(event, data)
